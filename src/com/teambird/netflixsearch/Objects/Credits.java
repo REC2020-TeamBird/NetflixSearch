b3d4a34fe2701bf6b11ce59808f8a8d12418f008
Janakan <janakan2466@gmail.com>
2020-11-27 19:52:54 -0500

    initial object initialization

--- src/com/teambird/netflixsearch/Objects/Credits.java
+++ src/com/teambird/netflixsearch/Objects/Credits.java
@@ -1,4 +1,28 @@
 package com.teambird.netflixsearch.Objects;
-
+import java.util.ArrayList;
 public class Credits {
-}
+    int Movie_id;
+    String Movie_title;
+    String title;
+
+/*cast
+    cast_id, character, credit_id, gender, id, name, order */
+
+/*crew
+    credit_it, department, gender, id, job, name,
+ */
+
+    public Credits (int Movie_id, String Movie_title, cast, crew){
+        this.Movie_id= Movie_id;
+        this.title= Movie_title;
+
+    }
+    private int movie(int Movie_id){
+
+    }
+    private String title(String title){
+
+    }
+    private String cast(int cast_id, String character, int credit_id, int gender, int id, String name,
+    }
+    private String
