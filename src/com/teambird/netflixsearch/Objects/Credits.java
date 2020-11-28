a8f08cac67417b96db5b5389307d7b77f202bde1
Janakan <janakan2466@gmail.com>
2020-11-27 20:20:26 -0500

    Credits edit 2

--- src/com/teambird/netflixsearch/Objects/Credits.java
+++ src/com/teambird/netflixsearch/Objects/Credits.java
@@ -12,17 +12,21 @@ public class Credits {
     credit_it, department, gender, id, job, name,
  */
 
-    public Credits (int Movie_id, String Movie_title, cast, crew){
+    public Credits (int Movie_id, String Movie_title, String cast, String crew){
         this.Movie_id= Movie_id;
         this.title= Movie_title;
 
     }
     private int movie(int Movie_id){
-
+    return 0;
     }
     private String title(String title){
-
+    return "";
     }
-    private String cast(int cast_id, String character, int credit_id, int gender, int id, String name,
+    private String cast(int cast_id, String character, int credit_id, int gender, int id, String name, int order) {
+        return "";
     }
-    private String
+    private String crew(String credit_id, String department, int gender, int id, String job, String name){
+        return "";
+    }
+}
